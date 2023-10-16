0f591c8a68604f6e037ab27e2d6da92458d32e6b
vburmus <burmusvolod@gmail.com>
2023-10-16 23:53:58 +0200

    Updated configuration to work with multipart files

--- src/main/java/com/epam/esm/utils/openfeign/CustomFeignClientConfiguration.java
+++ src/main/java/com/epam/esm/utils/openfeign/CustomFeignClientConfiguration.java
@@ -2,8 +2,13 @@ package com.epam.esm.utils.openfeign;
 
 import com.epam.esm.utils.exceptions.RestApiClientException;
 import com.epam.esm.utils.exceptions.RestApiServerException;
+import feign.codec.Encoder;
 import feign.codec.ErrorDecoder;
-import lombok.RequiredArgsConstructor;
+import feign.form.ContentType;
+import feign.form.MultipartFormContentProcessor;
+import feign.form.spring.SpringFormEncoder;
+import feign.form.spring.SpringManyMultipartFilesWriter;
+import feign.form.spring.SpringSingleMultipartFileWriter;
 import org.springframework.cloud.openfeign.support.JsonFormWriter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
@@ -12,8 +17,18 @@ import org.springframework.http.HttpStatus;
 import static com.epam.esm.utils.AuthConstants.*;
 
 @Import(JsonFormWriter.class)
-@RequiredArgsConstructor
 public class CustomFeignClientConfiguration {
+
+    @Bean
+    Encoder feignEncoder(JsonFormWriter jsonFormWriter) {
+        return new SpringFormEncoder() {{
+            var processor = (MultipartFormContentProcessor) getContentProcessor(ContentType.MULTIPART);
+            processor.addFirstWriter(jsonFormWriter);
+            processor.addFirstWriter(new SpringSingleMultipartFileWriter());
+            processor.addFirstWriter(new SpringManyMultipartFilesWriter());
+        }};
+    }
+
     @Bean
     public ErrorDecoder errorDecoder() {
         return (methodKey, response) -> {
@@ -28,5 +43,4 @@ public class CustomFeignClientConfiguration {
             }
         };
     }
-
 }
